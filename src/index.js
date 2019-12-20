3d5bcce8bec7d65648c4c4f8600fb159390f6472
François <frdebellescize@hotmail.fr>
2019-12-21 00:31:02 +0100

    update methode componentWillMount to UNSAFE_componentWillMount

--- src/index.js
+++ src/index.js
@@ -166,7 +166,7 @@ export default class Swipeable extends PureComponent {
     rightButtonsOpen: false
   };
 
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     const {onPanAnimatedValueRef, onRef} = this.props;
 
     onRef(this);
