7b0b63587397a4f08abdd45a7e65c99e3b0aa8d2
Marina Sheenkova <marina@astrails.com>
2020-06-09 12:48:17 +0300

    add useNativeDriver attribute

--- src/index.js
+++ src/index.js
@@ -145,7 +145,8 @@ export default class Swipeable extends PureComponent {
     swipeReleaseAnimationConfig: {
       toValue: {x: 0, y: 0},
       duration: 250,
-      easing: Easing.elastic(0.5)
+      easing: Easing.elastic(0.5),
+      useNativeDriver: false
     },
 
     // misc
@@ -204,7 +205,7 @@ export default class Swipeable extends PureComponent {
   _handlePan = Animated.event([null, {
     dx: this.state.pan.x,
     dy: this.state.pan.y
-  }]);
+  }], {useNativeDriver: false});
 
   _handleMoveShouldSetPanResponder = (event, gestureState) => (
     Math.abs(gestureState.dx) > this.props.swipeStartMinDistance
